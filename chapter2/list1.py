803edf3a3c53958b8c0fa44d9adaa331092255eb
Lmi1017 <daymoon826@gmail.com>
2021-11-05 16:54:41 +0900

    2021.11.05

---
+++ chapter2/list1.py
@@ -0,0 +1,6 @@
+# 리스트의 모든 원소를 스캔하기
+
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i in range(len(x)):
+    print(f'x[{i}] = {x[i]}')
---
+++ chapter2/list2.py
@@ -0,0 +1,6 @@
+# 리스트 모든 원소를 enumerate() 함수로 스캔
+
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i, name in enumerate(x):
+    print(f'x[{i}] = {x[i]}')
