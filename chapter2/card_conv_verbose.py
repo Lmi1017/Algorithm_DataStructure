9d778e9b39cf3f0dff9631577569172002241b98
Lmi1017 <daymoon826@gmail.com>
2021-11-08 00:55:54 +0900

    2021.11.08

--- chapter2/card_conv_verbose.py
+++ chapter2/card_conv_verbose.py
@@ -1,12 +1,19 @@
-# Do it! 실습 2-7 [A] 10진수 정수값을 입력받아 2~36진수로 변환하여 출력하기
+# 10진수 정숫값을 입력받아 2~36진수로 변환하여 출력하기
 
 def card_conv(x: int, r: int) -> str:
     """정수 x를 r 진수로 변환한 뒤 그 수를 나타내는 문자열을 반환"""
 
     d = ''  # 변환 뒤 문자열
     dchar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    n = len(str(x))  # 변환하기 전의 자릿수
 
+    print(f'{r:2} | {x:{n}d}')
     while x > 0:
+        print('   +'+(n+2)*'-')
+        if x//r:
+          print(f'{r:2} | {x//r:{n}d} ... {x%r}')
+        else:
+          print(f'    {x//r:{n}d} ... {x%r}')
         d += dchar[x % r]  # 해당하는 문자를 꺼내 결합
         x //= r
 
