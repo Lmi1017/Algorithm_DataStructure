94f698ff27ad8d97a3d8623a29b51b410bba87c3
Lmi1017 <daymoon826@gmail.com>
2022-01-03 04:09:32 +0900

    2022.01.03

---
+++ chpater4/last_element.py
@@ -0,0 +1,18 @@
+n = int(input('정수를 몇 개 저장할까요? : '))
+a = [None] * n  # 입력 받은 값을 저장하는 배열
+
+cnt = 0         # 입력 받은 개수
+while True:
+    a[cnt % n] = int(input((f'{cnt + 1} 번째 정수를 입력하세요.: ')))
+    cnt += 1
+
+    retry = input(f'계속 할까요?(Y ... Yes / N ... No) : ')
+    if retry in {'N', 'n'}:
+        break
+
+i = cnt - n
+if i < 0: i = 0
+
+while i < cnt:
+    print(f'{i + 1}번째 = {a[i % n]}')
+    i += 1
