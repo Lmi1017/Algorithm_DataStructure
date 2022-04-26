5aa91174836a23aa8e46fe0e5d46146b6afa8a2f
Lmi1017 <daymoon826@gmail.com>
2022-04-27 03:33:17 +0900

    2022.04.27

---
+++ chapter6/slection_sort.py
@@ -0,0 +1,27 @@
+from typing import MutableSequence
+
+
+def selection_sort(a: MutableSequence) -> None:
+    n = len(a)
+    for i in range(n-1):
+        min = i
+        for j in range(i+1, n):
+            if a[j] < a[min]:
+                min = j
+
+        a[i], a[min] = a[min], a[i]
+
+
+if __name__ == '__main__':
+    print('버블 정렬을 수행합니다.')
+    num = int(input('원소 수를 입력하세요.: '))
+    x = [None] * num  # 원소 수가 num인 배열을 생성
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: '))
+
+    selection_sort(x)  # 배열 x를 버블 정렬
+
+    print('오름차순으로 정렬했습니다.')
+    for i in range(num):
+        print(f'x[{i}] = {x[i]}')
